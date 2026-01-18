886b4394aa5bc3d762e343b64a51ec3a3339874d
Ahnaf Tahmid <sultantahmid.contact@gmail.com>
2026-01-18 15:01:51 +0600

    Step 1: Initial WinForms project setup

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParkingLotManagementSystem
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parking Lot Management System Started");
+            Console.ReadKey();
+        }
+    }
+}
