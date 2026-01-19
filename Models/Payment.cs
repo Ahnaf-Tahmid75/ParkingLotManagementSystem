535d7eb0ddac21b5085d69ca87fd689cebe0f651
Ahnaf Tahmid <ahnaftahmid.contact@gmail.com>
2026-01-19 18:03:01 +0600

    Step 2: Add Payment model

---
+++ Models/Payment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ParkingLotManagementSystem.Models
+{
+    public class Payment
+    {
+        public int PaymentID { get; private set; }
+        public ParkingTicket ParkingTicket { get; private set; }
+
+        public double TotalHours { get; private set; }
+        public decimal RatePerHour { get; private set; }
+        public decimal TotalAmount { get; private set; }
+
+        public DateTime PaymentTime { get; private set; }
+
+        public Payment(
+            int paymentId,
+            ParkingTicket parkingTicket,
+            double totalHours,
+            decimal ratePerHour)
+        {
+            PaymentID = paymentId;
+            ParkingTicket = parkingTicket;
+            TotalHours = totalHours;
+            RatePerHour = ratePerHour;
+            TotalAmount = (decimal)totalHours * ratePerHour;
+            PaymentTime = DateTime.Now;
+        }
+    }
+}
