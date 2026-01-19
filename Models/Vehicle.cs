bc0d169c1e42ece26767e04c2ae5627645081188
Ahnaf Tahmid <ahnaftahmid.contact@gmail.com>
2026-01-19 17:34:24 +0600

    Step 2: Add Vehicle and ParkingSlot models

---
+++ Models/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿namespace ParkingLotManagementSystem.Models
+{
+    public class Vehicle
+    {
+        public string LicensePlate { get; private set; }
+        public string VehicleType { get; private set; }
+
+        public Vehicle(string licensePlate, string vehicleType)
+        {
+            LicensePlate = licensePlate;
+            VehicleType = vehicleType;
+        }
+    }
+}
+
