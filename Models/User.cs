b7aca0af1f676b1df1eeac1bcca1d086944a0a7c
Ahnaf Tahmid <ahnaftahmid.contact@gmail.com>
2026-01-19 17:47:35 +0600

    Step 2: Add User model

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace ParkingLotManagementSystem.Models
+{
+    public class User
+    {
+        public int UserID { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Role { get; private set; } // Admin / Staff
+
+        public User(int userId, string username, string password, string role)
+        {
+            UserID = userId;
+            Username = username;
+            Password = password;
+            Role = role;
+        }
+    }
+}
+
