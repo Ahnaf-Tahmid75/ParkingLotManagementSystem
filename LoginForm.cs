891434fdd6decbe086233872cb8d708f18da48fb
Ahnaf Tahmid <ahnaftahmid.contact@gmail.com>
2026-01-20 13:50:31 +0600

    Step 4: Add LoginForm UI

---
+++ LoginForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using ParkingLotManagementSystem.Services;
+using ParkingLotManagementSystem.Models;
+
+namespace ParkingLotManagementSystem
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            AuthService authService = new AuthService();
+            User user = authService.Login(username, password);
+
+            if (user == null)
+            {
+                MessageBox.Show("Invalid username or password", "Login Failed");
+                return;
+            }
+
+            MessageBox.Show($"Welcome {user.Username} ({user.Role})", "Login Success");
+        }
+    }
+}
+
+
